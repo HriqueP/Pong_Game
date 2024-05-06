259eae4428e09104844992edeea2b47dc2543fd8
HriqueP <hapoletti@gmail.com>
2024-05-06 17:35:47 -0300

    canvas and players on-screen

--- js/main.js
+++ js/main.js
@@ -6,7 +6,7 @@ const ctx = myCanvas.getContext("2d");
 
 // Define the configs for the canvas, players and ball elements
 const config = {
-  canvasWidth: 1000,
+  canvasWidth: 1200,
   canvasHeight: 800,
   playerWidth: 10,
   playerHeight: 80,
@@ -16,3 +16,19 @@ const config = {
 // Draw the canvas
 canvas.width = config.canvasWidth;
 canvas.height = config.canvasHeight;
+
+// Define players objects
+const player1 = new Player(1); // Left side player
+const player2 = new Player(-1); // Right side player
+
+// Render Game
+function renderGame() {
+  player1.render();
+  player2.render();
+}
+
+// Function to call the other funcions (creating a loop for animation and updating the canvas)
+function animate() {
+  renderGame();
+}
+animate();
--- js/player.js
+++ js/player.js
@@ -1,2 +1,16 @@
-// Export the class which has all the player controls
-export default class Player {}
+class Player {
+  // Direction indicates the left and right side players
+  constructor(direction) {
+    this.direction = direction;
+    direction === 1
+      ? (this.x = 0 + 50)
+      : (this.x = config.canvasWidth - config.playerWidth - 50);
+    this.y = config.canvasHeight / 2 - config.playerHeight / 2;
+    this.score = 0;
+  }
+
+  render() {
+    ctx.fillStyle = "#ffffff";
+    ctx.fillRect(this.x, this.y, config.playerWidth, config.playerHeight);
+  }
+}
